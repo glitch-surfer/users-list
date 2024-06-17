2a44eac5214d4f19695d7aaf0460dd81f7f640ed
vladislav-garaev <marker661@gmail.com>
2024-06-17 21:54:49 +0400

    feat: implement loading between routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'users-list';
+export class AppComponent implements OnInit {
+  private readonly router = inject(Router);
+
+  private readonly isLoading$$ = new BehaviorSubject<boolean>(false);
+  readonly isLoading$ = this.isLoading$$.asObservable();
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        tap((event) => {
+          switch (true) {
+            case event instanceof NavigationStart: {
+              this.isLoading$$.next(true);
+              break;
+            }
+
+            case event instanceof NavigationEnd:
+            case event instanceof NavigationCancel:
+            case event instanceof NavigationError: {
+              this.isLoading$$.next(false);
+              break;
+            }
+            default: {
+              break;
+            }
+          }
+        }),
+        takeUntilDestroyed()
+      )
+      .subscribe();
+  }
 }
